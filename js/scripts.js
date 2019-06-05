bc9f28ce8b287d75b4b9697b2703858c922c919c
keniamoya <ke.moya.mora@gmail.com>
2019-06-05 01:53:14 -0600

    KM:: Update interface

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$('#recipeCarousel').carousel({
+    interval: 5000
+})
+
+$('.carousel .carousel-item').each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length > 0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+});
