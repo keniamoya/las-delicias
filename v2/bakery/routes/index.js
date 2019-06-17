1969a4e71372c165043819152e100f7aad1f6695
Necrotavo <gapalomo.u@gmail.com>
2019-06-17 16:55:59 -0600

    Update index.js
    Solo metí un espacio para probar

--- v2/bakery/routes/index.js
+++ v2/bakery/routes/index.js
@@ -17,6 +17,7 @@ router.get('/', function(req, res, next) {
     })
 });
 
+
 /* GET home page. 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Las delicias', category: 'category' });
