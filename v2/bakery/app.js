5b71f23b30f8a601ec8ccf44c65daa1eee64f2d5
keniamoya <ke.moya.mora@gmail.com>
2019-06-19 12:38:59 -0600

    KM:: Update style

--- v2/bakery/app.js
+++ v2/bakery/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var cartRouter = require('./routes/cart');
 var productRouter = require('./routes/product');
+var usRouter = require('./routes/us');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/cart', cartRouter);
 app.use('/product', productRouter);
+app.use('/us', usRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
